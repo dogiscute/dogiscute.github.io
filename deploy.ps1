86cbbc3d03e78d09d188d8cf17f43bcf13c01df0
dogiscute <dogiscute@gmail.com>
2024-03-20 12:10:09 +0800

    update blog

--- deploy.ps1
+++ deploy.ps1
@@ -1,4 +1,5 @@
 git config --global --add safe.directory D:/Desktop/dogiscute/blog/.deploy_git
+hexo g
 
 git add .
 git commit -m "update blog"
--- source/about/index.md
+++ source/about/index.md
@@ -3,4 +3,4 @@ title: about
 date: 2023-03-29 19:47:32
 ---
 
-20240320 測試SSSSSS
+20240320 KKKKKKK
